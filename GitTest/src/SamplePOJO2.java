25e391e4be2cf865fa8fc0018229f6179f9f85f3
ss <dd@gd.ghjg>
2016-05-18 19:09:04 +0530

    jjjjjjjjjjj new class added

---
+++ GitTest/src/SamplePOJO2.java
@@ -0,0 +1,15 @@
+
+public class SamplePOJO2
+{
+   private String name;
+
+   public String getName()
+   {
+      return name;
+   }
+
+   public void setName(String name)
+   {
+      this.name = name;
+   }
+}
