3569012e56b97bc6f410ad116d1d2875810a4ea0
ss <dd@gd.ghjg>
2016-05-18 14:45:10 +0530

    rrrrrrrr

--- GitTest/src/SamplePOJO.java
+++ GitTest/src/SamplePOJO.java
@@ -1,25 +1,10 @@
 
 public class SamplePOJO
 {
-   private int    id;
    private String name;
    
 
-   public int getId()
-   {
-      return id;
-   }
 
-   public void setId(int id)
-   {
-      this.id = id;
-   }
-
-   @Override
-   public String toString()
-   {
-      return "SamplePOJO [id=" + id + ", name=" + name + "]";
-   }
 
    public String getName()
    {
